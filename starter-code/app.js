2004ceafd1b0e139a78480f2d17c899a22f48735
twardcox <t.ward.cox@gmail.com>
2019-06-08 07:28:42 -0700

    finished multiplyArray

--- starter-code/app.js
+++ starter-code/app.js
@@ -96,8 +96,8 @@ function sumArray(sumArr) {
   for (var i = 0; i < sumArr.length; i++) {
     summed = sum(summed, sumArr[i])[0];
 
-    console.log('summed: ', summed);
-    console.log('sumArr[i]: ', sumArr[i]);
+    // console.log('summed: ', summed);
+    // console.log('sumArr[i]: ', sumArr[i]);
   }
   //
   // 2,3,4 was passed in as an array of numbers, and 9 is their sum.
@@ -106,7 +106,7 @@ function sumArray(sumArr) {
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -123,10 +123,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) {
   //eslint-disable-line
+  var multiples = 1;
+  for (var j = 0; j < multArr.length; j++) {
+    multiples = multiply(multiples, multArr[j])[0];
+    // console.log('multiples: ', multiples);
+  }
+  return [multiples, 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + multiples + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
